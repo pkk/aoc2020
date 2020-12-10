697f8cb75bbfbf4e01b1d15dbef23f94696596cd
KKP <krishnakamal@gmail.com>
2020-12-10 14:58:53 +0530

    Day 10 P1

--- Day10/Day10.hs
+++ Day10/Day10.hs
@@ -1,4 +1,13 @@
+import Data.List
+
 main :: IO ()
 main = do
-    input <- map (read :: String -> Int) <$> lines <$> readFile "input.txt"
-    print $ input !! 0
+    input <- sort <$> map (read :: String -> Int) <$> lines <$> readFile "input.txt"
+    print $ day10p1 $ input
+
+day10p1 :: [Int] -> Int
+day10p1 xs = 
+    let
+        (a,b,c) = foldl (\(x,y,z) ele -> if ele - x == 1 then (ele,y+1,z) else (ele,y,z+1)) (0,0,1) xs
+    in
+        c * b
