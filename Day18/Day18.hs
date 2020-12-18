4b9d69f820d6435e6f74f82d0f3b1755f1d80b01
KKP <krishnakamal@gmail.com>
2020-12-18 22:01:19 +0530

    added day18 p1 & p2

---
+++ Day18/Day18.hs
@@ -0,0 +1,68 @@
+import Text.Read
+import Data.List.Split
+
+data Expression = Node (Int -> Int -> Int) Expression Expression | Leaf Int
+
+instance Show Expression where
+    show (Node f e1 e2) = let op = if f 1 1 == 2 then "+" else "*" in "(" ++ op ++ " " ++ show e1 ++ " " ++ show e2 ++ ")"
+    show (Leaf i) = show i
+
+
+parse1 :: String -> Expression
+parse1 input = parse' (filter (/= ' ') $ reverse input)
+    where parse' str =
+            case maybeSplit "*+" str of
+                Just (str1, op, str2) -> Node (toFunc op) (parse' str1) (parse' str2)
+                Nothing -> case str of
+                    (')':rest) -> parse' $ reverse $ tail $ reverse rest
+                    (i:[]) -> Leaf $ read [i]
+
+parse2 :: String -> Expression
+parse2 input = parse' (filter (/= ' ') $ reverse input)
+    where parse' str = 
+            case maybeSplit "*" str of
+                Just (str1, op, str2) -> Node (toFunc op) (parse' str1) (parse' str2)
+                Nothing -> case maybeSplit "+" str of
+                    Just (str1, op, str2) -> Node (toFunc op) (parse' str1) (parse' str2)
+                    Nothing -> case str of
+                        (')':rest) -> parse' $ reverse $ tail $ reverse rest
+                        (i:[]) -> Leaf $ read [i]
+
+
+maybeSplit :: String -> String -> Maybe (String, Char, String)
+maybeSplit = maybeSplit' "" 0
+    where maybeSplit' _ _ _ [] = Nothing
+          maybeSplit' acc i splitters (c:s)
+            | c `elem` splitters && i == 0 = Just (acc, c, s)
+            | (c == ')') = maybeSplit' (acc ++ [c]) (i+1) splitters s
+            | (c == '(') = maybeSplit' (acc ++ [c]) (i-1) splitters s
+            | otherwise = maybeSplit' (acc ++ [c]) i splitters s
+
+main :: IO ()
+main = do
+    input <- splitFile "input.txt" "\n"
+    let tot = sum $ map (evaluate . parse1) input
+    print tot
+    print $ sum $ map (evaluate . parse2) input
+
+
+evaluate :: Expression -> Int
+evaluate (Node op e1 e2) = op (evaluate e1) (evaluate e2)
+evaluate (Leaf i) = i
+
+
+toFunc :: Char -> Int -> Int -> Int
+toFunc '+' = (+)
+toFunc '*' = (*)
+
+splitFile :: String -> String -> IO [String]
+splitFile name splitter = do
+        input <- readFile name
+        let filtered = filter (\x -> x /= "") $ splitOn splitter input
+        return filtered
+
+splitAndReadFile :: Read a => String -> String -> IO [a]
+splitAndReadFile name splitter = do
+        input <- readFile name
+        let filtered = filter (\x -> x /= "") $ splitOn splitter input
+        return $ map read filtered
