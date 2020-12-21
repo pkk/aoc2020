1c6a8b05b31cda548b05c588eef7063d56184011
KKP <krishnakamal@gmail.com>
2020-12-21 23:24:27 +0530

    added Day21 P2

--- Day21/Day21P2.hs
+++ Day21/Day21P2.hs
@@ -22,16 +22,17 @@ main = do
     let potentialIngredients = nub $ concat $ (M.elems allergyMap)
     let safeIngredients = filter (\i -> not (i `elem` potentialIngredients)) allIngredients
     print $ length $ filter (`elem` safeIngredients) $ concat $ map ingredients input
-    let final = concat $ intersperse "," (map snd (sort $ run2 allergyMap))
-    putStrLn final
 
-run2 :: M.Map String [String] -> [(String, String)]
-run2 a2f = run' a2f (M.toList a2f)
-    where run' full ((allergen, foods):a2f) = 
-           case foods of
-            food:[] -> (allergen, food) : (run2 $ M.map (filter (/= food)) full)
-            _ -> run' full a2f
-          run' full [] = []
+run :: M.Map String [String] -> [Food] -> M.Map String [String]
+run a2f ((Food ingredients allergens):foods) = run (updateMap a2f ingredients allergens) foods
+    where updateMap a2f ingredients (allergen:allergens) = 
+            let updated = 
+                 case M.lookup allergen a2f of 
+                    Just (ingredients') -> M.insert allergen (ingredients `intersect` ingredients') a2f
+                    Nothing -> M.insert allergen ingredients a2f
+            in updateMap updated ingredients allergens
+          updateMap a2f ingredients [] = a2f
+run a2f _ = a2f
 
 splitFile :: String -> String -> IO [String]
 splitFile name splitter = do
